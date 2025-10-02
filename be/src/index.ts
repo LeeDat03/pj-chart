c5f9cc0f2efdcac152972cd6732f3e5d911e0bf5
LeeDat03 <leedat104517@gmail.com>
2025-10-02 11:04:34 +0700

    update be cors config for fe

--- be/src/index.ts
+++ be/src/index.ts
@@ -11,8 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    // TODO: change this
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:3001",
     credentials: true,
   })
 );
